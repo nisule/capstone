a686c0cb44f786ec0a90f2e5e4cdc724d09ba664
Nicholas Sundvall <sundvanl@plu.edu>
2019-11-07 23:51:34 -0800

    Added list of example items to DrinkMenu.js

--- additionalPages/DrinkMenu.js
+++ additionalPages/DrinkMenu.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Text, StyleSheet, View} from 'react-native';
+import { Text, StyleSheet, View, TouchableHighlight, FlatList} from 'react-native';
 import FooterView from './Footer.js';
 
 export default class DrinkMenu extends Component {
@@ -7,6 +7,11 @@ export default class DrinkMenu extends Component {
     super(props);
     this.state = {text: ''};
   }
+  
+  addToOrder(){
+    alert("Added item to order")
+  }
+
   render() {
     return (
         <View style={{flex: 1, backgroundColor: '#181818'}}>
@@ -14,10 +19,34 @@ export default class DrinkMenu extends Component {
             <View style={{flex: 1, alignItems: 'center'}}>
                 <Text style={{fontSize: 30, color: 'white'}}>Drinks</Text>
             </View>
+
+            <View style={styles.container}>
+              <FlatList
+                data={[
+                  {key: 'Caramel Frap'},
+                  {key: 'Frap Mocha'},
+                  {key: 'Frap Vanilla'},
+                  {key: 'Frap Coffee'},
+                  {key: 'Raspberry Rockstar'},
+                  {key: 'Sweet Tea'},
+                  {key: 'Raspberry Tea'},
+                  {key: 'Cappuccino'},
+                  {key: 'Macchiato'},
+                  {key: 'Iced Coffee'},
+                  {key: 'Iced Americano'},
+                  {key: 'Hot Chocolate'}
+                ]}
+                renderItem={({item}) => {
+                  return(
+                    <TouchableHighlight onPress={() => this.addToOrder()}>
+                      <Text style={styles.item}>{item.key}</Text>
+                    </TouchableHighlight>
+                  )
+                  }
+                }
+              />
+            </View>
           
-
-
- 
             {/* <View style={{flex: 1, justifyContent: 'flex-end'}}>
                 <FooterView/>
             </View> */}
@@ -28,5 +57,14 @@ export default class DrinkMenu extends Component {
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+   flex: 25,
+   paddingTop: 10
+  },
+  item: {
+    padding: 10,
+    fontSize: 26,
+    height: 60,
+    color: 'white'
+  },
 })
