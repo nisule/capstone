685d3879b7137c8c7c0001234decdfdb6d570956
Sam Driver <SamJDriv@gmail.com>
2019-11-21 15:33:39 -0800

    Created vs project, connected to DB

--- backend/CafeApplication/Database/DBAccess.cs
+++ backend/CafeApplication/Database/DBAccess.cs
@@ -5,46 +5,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
+using System.Data;
 
-
-class Program { 
+class DBAccess {
     static void Main(string[] args) {
+
+
 
         Console.WriteLine("Getting Connection ...");
         MySqlConnection conn = createConnection();
 
-        try
-        {
+        try {
             Console.WriteLine("Opening Connection ...");
 
             conn.Open();
 
             Console.WriteLine("Connection successful!");
-            String sql = "INSERT INTO Item(item_name, stock, price, is_drink, addition_date) VALUES('Americano', 47, 3.49, true, '2019-11-20')";
-            MySqlCommand command = new MySqlCommand(sql, conn);
-            command.ExecuteReader();
+            //String sql = "INSERT INTO Item(item_name, stock, price, is_drink, addition_date) VALUES('Cappuccino', 30, 3.99, true, '2019-11-20')";
+            String sql = "SELECT * FROM Item";
 
+            MySqlCommand command = new MySqlCommand(sql, conn);
+            //command.ExecuteReader();
+            var dataReader = command.ExecuteReader();
+            var dataTable = new DataTable();
+            dataTable.Load(dataReader);
             conn.Close();
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             Console.WriteLine("Error: " + e.Message);
         }
 
         Console.Read();
     }
 
-    public static MySqlConnection createConnection()
-    {
-        // Insert credentials and connection details
-        string host = "";
-        int port = 0;
-        string database = "";
-        string username = "";
-        string password = "";
+    public static MySqlConnection createConnection() {
+        string host = "mal.cs.plu.edu";
+        int port = 3306;
+        string database = "kc499_2019";
+        string username = "kc499";
+        string password = "KelleyCafe";
 
         // Connection String.
-        // todo string format this instead of concat
         String connString = "Server=" + host + ";Database=" + database
             + ";port=" + port + ";User Id=" + username + ";password=" + password;
 
