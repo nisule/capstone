7f3b9344a6fbeca0713974ce99e5e6bd9ab08484
Sam Driver <SamJDriv@gmail.com>
2019-11-24 00:25:33 -0800

    Refactors orders code

--- backend/CafeApplication/Database/DBAccess.cs
+++ backend/CafeApplication/Database/DBAccess.cs
@@ -8,43 +8,30 @@ using System.Globalization;
 using MySql.Data.MySqlClient;
 using System.Data;
 
-public class DBAccess {
+public static class DBAccess {
 
-    private static MySqlConnection connection;
+    private static string database = "kc499_2019";
+    private static string username = "kc499";
+    private static string password = "KelleyCafe";
+
+    //On-campus host & port variables
+    //private static string host = "mal.cs.plu.edu";
+    //private static int port = 3306;
 
 
-    public DBAccess() {
-        //string host = "mal.cs.plu.edu";
-        //int port = 3306;
-        string database = "kc499_2019";
-        string username = "kc499";
-        string password = "KelleyCafe";
+    // Off-campus connection host & port variables:
+    private static string host = "localhost";
+    private static int port = 2000;
 
-        // Off-campus connection host & port variables:
-        string host = "localhost";
-        int port = 2000;
-
-        // Connection String.
-        String connString = "Server=" + host + ";Database=" + database
+    private static string connString = "Server=" + host + ";Database=" + database
             + ";port=" + port + ";User Id=" + username + ";password=" + password;
 
-        MySqlConnection conn = new MySqlConnection(connString);
+    private static MySqlConnection connection = new MySqlConnection(connString);
 
-        connection = conn;
-    }
-
-    public static void Main(string[] args) {
-        var db = new DBAccess();
-        var table = getAllDrinks();
-
-
-        Console.Read();
-    }
-
+    //All items for main menu
     public static DataTable getAllItems() {
         try {
             connection.Open();
-            //String sql = "INSERT INTO Item(item_name, stock, price, is_drink, addition_date) VALUES('Cappuccino', 30, 3.99, true, '2019-11-20')";
             String sql = "SELECT * FROM Item";
 
             MySqlCommand command = new MySqlCommand(sql, connection);
@@ -55,14 +42,15 @@ public class DBAccess {
             return dataTable;
         }
         catch (Exception e) {
-            Console.WriteLine("Error: " + e.Message);
+            Debug.WriteLine("Error in retrieving all items: " + e.Message);
             return null;
         }
     }
+
+    //Only drinks filter
     public static DataTable getAllDrinks() {
         try {
             connection.Open();
-            //String sql = "INSERT INTO Item(item_name, stock, price, is_drink, addition_date) VALUES('Cappuccino', 30, 3.99, true, '2019-11-20')";
             String sql = "SELECT * FROM Item WHERE is_drink = true";
 
             MySqlCommand command = new MySqlCommand(sql, connection);
@@ -73,81 +61,82 @@ public class DBAccess {
             return dataTable;
         }
         catch (Exception e) {
-            Console.WriteLine("Error: " + e.Message);
+            Debug.WriteLine("Error in retrieving drinks: " + e.Message);
+            return null;
+        }
+    }
+
+    public static DataTable getItemPrice(string itemID) {
+        try {
+            connection.Open();
+            String sql = "SELECT price FROM Item WHERE item_id = " + itemID;
+
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            var dataReader = command.ExecuteReader();
+            var dataTable = new DataTable();
+            dataTable.Load(dataReader);
+            connection.Close();
+            return dataTable;
+        }
+        catch (Exception e) {
+            Debug.WriteLine("Error in retrieving single item price: " + e.Message);
+            return null;
+        }
+    }
+
+    public static void insertNewOrder(string userID, double total) {
+        try {
+            connection.Open();
+            String sql = "INSERT INTO Orders(user_id, total) " +
+                         "VALUES(" + userID + ", " + total + ")";
+
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            var dataReader = command.ExecuteReader();
+            connection.Close();
+        }
+        catch (Exception e) {
+            Debug.WriteLine("Error inserting order: " + e.Message);
+        }
+    }
+
+    public static void insertOrderWithItem(string orderID, int itemID, int itemQuantity) {
+        try {
+            connection.Open();
+            String sql = "INSERT INTO Order_Items(order_id, item_id, quantity) " +
+                         "VALUES(" + orderID + ", " + itemID + ", " + itemQuantity + ")";
+
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            var dataReader = command.ExecuteReader();
+            connection.Close();
+        }
+        catch (Exception e) {
+            Debug.WriteLine("Error inserting order: " + e.Message);
+        }
+    }
+
+    public static DataTable getUserLatestOrder(string userID) {
+        try {
+            connection.Open();
+            String sql = "SELECT order_id " +
+                         "FROM Orders " + 
+                         "WHERE user_id = 11111111 " +
+                         "ORDER BY order_id DESC LIMIT 1";
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            var dataReader = command.ExecuteReader();
+            var dataTable = new DataTable();
+            dataTable.Load(dataReader);
+            connection.Close();
+            return dataTable;
+        }
+        catch (Exception e) {
+            Debug.WriteLine("Error in retrieving single item price: " + e.Message);
             return null;
         }
     }
 
 
 
-}
 
-    /*
-     * This method will take in the user_id of the User, and a dictionary of item_id as key and quantity as value. It will then
-     * create the appropriate Orders and use the order_id of that entry to create the data in the Order_Items table.
-     */
-    public static void createOrder(string user_id, Dictionary<int, int> items, MySqlConnection conn)
-    {
-        /*
-         * Need to calculate the total, and create a new row in Orders table. Then need to get the order_id of that entry
-         * and use that to create all the entries for the OrderItems table.
-         */
-        double taxRate = 0.102;
-        int number_of_items = items.Count;
-        Console.WriteLine(number_of_items);
-
-        double total = 0;
-
-        // Calculating total:
-        for(int i = 0; i < number_of_items; i++)
-        {
-            string itemId = items.ElementAt(i).Key.ToString();
-            int itemQuantity = items.ElementAt(i).Value;
-            string sql = "SELECT * FROM Item WHERE item_id = " + itemId;
-
-            MySqlCommand command = new MySqlCommand(sql, conn);
-            var dataReader = command.ExecuteReader();
-            dataReader.Read();
-
-            total += Convert.ToDouble(dataReader["price"]) * itemQuantity;
-            Console.WriteLine(total);
-
-            dataReader.Close();
-        }
-
-        total += total * taxRate;
-
-        // Insert order into Orders table.
-        string addOrderToTable = "INSERT INTO Orders(user_id, total) VALUES("+ user_id + ", " + total + ")";
-        Console.WriteLine(addOrderToTable);
-
-        MySqlCommand addOrder = new MySqlCommand(addOrderToTable, conn);
-        var orderSubmitter = addOrder.ExecuteReader();
-
-        orderSubmitter.Close();
-
-        // Selects the row with the largest order_id, which is the order that was just added.
-        string selectOrderString = "SELECT order_id from Orders ORDER BY order_id DESC LIMIT 1;";
-        MySqlCommand selectOrder = new MySqlCommand(selectOrderString, conn);
-        var orderSelecter = selectOrder.ExecuteReader();
-        orderSelecter.Read();
-        string orderID = orderSelecter["order_id"].ToString();
-        orderSelecter.Close();
-        Console.WriteLine(orderID);
-
-        // Inserting each order item to the OrderItems table.
-        for(int i = 0; i < number_of_items; i++)
-        {
-            int itemID = items.ElementAt(i).Key;
-            int itemQuantity = items.ElementAt(i).Value;
-            string sql = "INSERT INTO Order_Items(order_id, item_id, quantity) " +
-                         "VALUES(" + orderID + ", " + itemID + ", " + itemQuantity + ")";
-
-            MySqlCommand command = new MySqlCommand(sql, conn);
-            var dataReader = command.ExecuteReader();
-
-            dataReader.Close();
-        }
-    }
 
 }
+
---
+++ backend/CafeApplication/OrderHandling/OrderCreator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderHandling {
+    public class OrderCreator {
+        
+        public static void Main(string[] args) {
+            OrderCreator o = new OrderCreator();
+
+            string user_id = "11111111";
+            // Dictionary with item_id and quantity.
+            Dictionary<int, int> items = new Dictionary<int, int>();
+
+            //Test example
+            items.Add(26, 2);
+            items.Add(1, 2);
+            items.Add(54, 1);
+            items.Add(19, 36);
+
+            o.ProcessOrder(user_id, items);
+            
+        }
+
+       /*
+        * This method will take in the user_id of the User, and a dictionary of item_id as key and quantity 
+        * as value. It will then create the appropriate Orders and use the order_id of that entry to create 
+        * the data in the Order_Items table.
+        */
+        public void ProcessOrder(string user_id, Dictionary<int, int> items) {
+            //Computer the total and insert it into the database
+            double orderTotal = computeTotal(items, .102);
+            DBAccess.insertNewOrder(user_id, orderTotal);
+            
+            //Get the ID of the user's order they just placed
+            var table = DBAccess.getUserLatestOrder(user_id);
+            string orderID = table.Rows[0].ItemArray[0].ToString();
+
+            //Insert each item in the order into the database
+            foreach (KeyValuePair<int, int> entry in items) {
+                DBAccess.insertOrderWithItem(orderID, entry.Key, entry.Value);
+            }
+
+        }
+
+        private double computeTotal(Dictionary<int, int> items, double taxRate) {
+            double total = 0;
+
+            // Calculating total:
+            for (int i = 0; i < items.Count; i++) {
+                string itemId = items.ElementAt(i).Key.ToString();
+                int itemQuantity = items.ElementAt(i).Value;
+                total += getItemPrice(itemId) * itemQuantity;
+            }
+            total += total * taxRate;
+            return total;
+        }
+
+        private double getItemPrice(string itemID) {
+            var dataTable = DBAccess.getItemPrice(itemID);
+
+            if (dataTable.Rows.Count != 1)
+                return -1;
+
+            return double.Parse(dataTable.Rows[0].ItemArray[0].ToString());
+
+        }
+    }
+}
