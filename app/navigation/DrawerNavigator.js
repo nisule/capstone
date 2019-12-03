06a025af859b858d0c0d03bdd1163084592187d7
garciama <garciama@plu.edu>
2019-12-03 15:36:18 -0800

    Added back menu

--- app/navigation/DrawerNavigator.js
+++ app/navigation/DrawerNavigator.js
@@ -3,12 +3,16 @@ import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer, createNavigator } from 'react-navigation';
 
 import loginView from '../additionalPages/Login';
+import menuView from '../additionalPages/MainMenu';
 import AccountSettings from '../additionalPages/Account';
 import AppSettings from '../additionalPages/AppSettings';
 
 const WIDTH = Dimensions.get("window").width;
 
 const DrawerNavigator = createDrawerNavigator({
+      Menu: {
+        screen: menuView,
+      },
       Account: {
         screen: AccountSettings,
       },
