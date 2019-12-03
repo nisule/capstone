88b79f72ea9c5c7819f892018fc37f279fb6a0f1
garciama <garciama@plu.edu>
2019-12-03 15:25:31 -0800

    Removed menu from hamburger menu

--- app/navigation/DrawerNavigator.js
+++ app/navigation/DrawerNavigator.js
@@ -3,16 +3,12 @@ import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer, createNavigator } from 'react-navigation';
 
 import loginView from '../additionalPages/Login';
-import menuView from '../additionalPages/MainMenu';
 import AccountSettings from '../additionalPages/Account';
 import AppSettings from '../additionalPages/AppSettings';
 
 const WIDTH = Dimensions.get("window").width;
 
 const DrawerNavigator = createDrawerNavigator({
-      Menu: {
-        screen: menuView,
-      },
       Account: {
         screen: AccountSettings,
       },
