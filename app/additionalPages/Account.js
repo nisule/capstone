c81aa4572520c29f37ff9d4150cda4a9bdb5f866
garciama <garciama@plu.edu>
2019-12-28 17:06:34 -0800

    Added items to account and settings

--- app/additionalPages/Account.js
+++ app/additionalPages/Account.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
-import { Text, View, SafeAreaView} from 'react-native';
-import FooterView from './Footer';
+import { Alert, Button, Text, View, SafeAreaView, StyleSheet, Dimensions} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+
+var viewWidth = Dimensions.get('window').width; 
 
 class AccountSettings extends Component {
   constructor(props) {
     super(props);
-    this.state = {text: ''};
   }
   
   render() {
@@ -14,13 +15,70 @@ class AccountSettings extends Component {
     return (
         <SafeAreaView style={{flex: 1, backgroundColor: '#181818'}}>
 
-            <View style={{flex: 1, alignItems: 'center'}}>
-                <Text style={{fontSize: 30, color: 'white'}}>Your Account</Text>
+            <View style={styles.titleStyle}>
+                <Text style={styles.titleText}>Your Account</Text>
             </View>
+
+            <ScrollView>
+
+              <View style={styles.viewStyle}>
+                <Text style={styles.textStyle}>
+                  Email: ~Use API~
+                </Text>
+              </View>
+
+              <View style={styles.viewStyle}>
+                <Text style={styles.textStyle}>
+                  Dining Dollars: ~Use API~
+                </Text>
+              </View>
+
+              <View style={styles.viewStyle}>
+                <Text style={styles.textStyle}>
+                  ID Number: ~Use API~
+                </Text>
+              </View>
+
+            </ScrollView>
+
+            <View style={styles.buttonStyle}>
+                <Button
+                  title="Change Password"
+                  onPress={() => Alert.alert('Add popup window to create new password.')}
+                  color="#fbba37"
+                />
+              </View>
               
         </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  titleText:{
+    fontSize: 30, 
+    color: 'white'
+  },
+  titleStyle:{
+    marginBottom: 15,
+    backgroundColor:'grey',
+    alignItems:'center'
+  },
+  textStyle: {
+    color: 'white',
+    fontSize: 24
+  },
+  viewStyle: {
+    marginBottom: 15,
+    borderWidth: 1.5,
+    borderColor: 'grey',
+  },
+  buttonStyle: {
+    justifyContent: "flex-end",
+    marginLeft: viewWidth * 0.3,
+    marginRight: viewWidth * 0.3,
+    marginBottom: 10,
+  }
+})
 
 module.exports = AccountSettings
