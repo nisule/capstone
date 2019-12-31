1a470bb2efea5be9744a4e7f43c10de8c660d42f
garciama <garciama@plu.edu>
2019-12-30 18:31:25 -0800

    Remade food and drinks menus.

--- app/additionalPages/DrinkMenu.js
+++ app/additionalPages/DrinkMenu.js
@@ -1,64 +1,137 @@
-import React, {Component} from 'react';
-import { Text, StyleSheet, View, SafeAreaView, SectionList, TouchableOpacity} from 'react-native';
-import { Card } from 'react-native-elements';
-
-const data = [
-  {
-    title:"Your Favorites",
-    data:[
-      {key:1, name:"Chocolate Milk", image:require("./img/Chocolate_Milk.jpg"), price: "0.50"},
-    ]
-  },
-  {
-    title:"All Drinks",
-    data:[
-      {key:1, name:"Chocolate Milk", image:require("./img/Chocolate_Milk.jpg"), price: "0.50"},
-      {key:2, name:"Gatorade", image:require("./img/gatorade.jpg"), price: "1.89"},
-    ]
-  }
-]
+import React, {Component} from "react";
+import {FlatList, StyleSheet, View, SafeAreaView, ActivityIndicator} from "react-native";
+import {SearchBar, ListItem} from "react-native-elements";
 
 export default class DrinkMenu extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      loading: false,
+      data: [],
+      error: null,
+    };
+
+    // Delete this after API is working, just have this data to demonstrate how the views are working.
+    this.allDrinks = [
+      {key:1, name:"Chocolate Milk", image:require("./img/Chocolate_Milk.jpg"), price: "0.50"},
+      {key:2, name:"Gatorade", image:require("./img/gatorade.jpg"), price: "1.89"},
+      {key:3, name:"Chocolate Milk1", image:require("./img/Chocolate_Milk.jpg"), price: "0.50"},
+      {key:4, name:"Gatorade1", image:require("./img/gatorade.jpg"), price: "1.89"},
+      {key:5, name:"Chocolate Milk2", image:require("./img/Chocolate_Milk.jpg"), price: "0.50"},
+      {key:6, name:"Gatorade2", image:require("./img/gatorade.jpg"), price: "1.89"},
+      {key:7, name:"Chocolate Milk3", image:require("./img/Chocolate_Milk.jpg"), price: "0.50"},
+      {key:8, name:"Gatorade3", image:require("./img/gatorade.jpg"), price: "1.89"},
+      {key:9, name:"Chocolate Milk4", image:require("./img/Chocolate_Milk.jpg"), price: "0.50"},
+      {key:10, name:"Gatorade4", image:require("./img/gatorade.jpg"), price: "1.89"},
+      {key:11, name:"Chocolate Milk5", image:require("./img/Chocolate_Milk.jpg"), price: "0.50"},
+      {key:12, name:"Gatorade5", image:require("./img/gatorade.jpg"), price: "1.89"},
+      {key:13, name:"Chocolate Milk6", image:require("./img/Chocolate_Milk.jpg"), price: "0.50"},
+      {key:14, name:"Gatorade6", image:require("./img/gatorade.jpg"), price: "1.89"},
+      {key:15, name:"Chocolate Milk7", image:require("./img/Chocolate_Milk.jpg"), price: "0.50"},
+      {key:16, name:"Gatorade7", image:require("./img/gatorade.jpg"), price: "1.89"},
+    ];
   }
+
+  // This method is invoked once after the native UI for this component has finished rendering. This will
+  // automatically load all the drinks from the API once the screen is loaded.
+  componentDidMount(){
+    this.makeRemoteRequest();
+  }
+
+
+  makeRemoteRequest = () => {
+    this.setState({loading: true});
+
+    // Delete this statement after API is working.
+    this.setState({loading: false, data: this.allDrinks, error: null})
+
+    // Once we get the api set up, retrieve list of all drinks and save to local list:
+    // const url = `https://<url to get list of drinks>`;
+    // this.setState({ loading: true });
+
+    // fetch(url)
+    //   .then(res => res.json())
+    //   .then(res => {
+    //     this.setState({
+    //       data: res.results,
+    //       error: res.error || null,
+    //       loading: false,
+    //     });
+    //     this.allDrinks = res.results;
+    //   })
+    //   .catch(error => {
+    //     this.setState({ error, loading: false });
+    //   });
+  };
 
   addToOrder(){
     alert("TODO: Add navigation to popup to look at nutrition and add to order.")
   }
 
-  render() {
-		return (
-      <SafeAreaView style={{backgroundColor: 'grey'}}>
-        <SectionList
-            sections={data}
-            
-            // Generating section headers
-            renderSectionHeader={({section}) => {
-              return (
-                <View style={styles.titleContainer}>
-                  <Text style={styles.title}>
-                    {section.title}
-                  </Text>
-                </View>
-              )
-            }}
+  searchFilterFunction = text => {
+    this.setState({
+      value: text,
+    });
 
-            // Generating items for each section
-            renderItem={({item}) => {
-              return (
-                <TouchableOpacity  style={styles.cardBackground} onPress={() => this.addToOrder()}>
-                  <Card imageProps={styles.image} containerStyle={styles.cardContainer} titleStyle={styles.titleStyle}
-                  image={item.image}
-                  title={item.name}>
-                    <Text style={{textAlign: 'center', color:'#ffffff', fontSize: 20}}>
-                      Price: ${item.price}
-                    </Text>
-                  </Card>
-                </TouchableOpacity>
-              )
-            }}/>
+    const newData = this.allDrinks.filter(item => {
+      const itemData = `${item.name.toUpperCase()}`;
+      const textData = text.toUpperCase();
+
+      return itemData.indexOf(textData) > -1;
+    });
+    
+    this.setState({
+      data: newData,
+    });
+  };
+
+  renderHeader = () => {
+    return (
+      <SearchBar
+        placeholder="Type Here..."
+        onChangeText={text => this.searchFilterFunction(text)}
+        autoCorrect={false}
+        value={this.state.value}
+      />
+    );
+  };
+
+  renderSeparator = () => {
+    return (
+      <View
+        style={styles.separator}
+      />
+    );
+  };
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <SafeAreaView style={styles.loading}>
+          <ActivityIndicator />
+        </SafeAreaView>
+      );
+    }
+		return (
+      <SafeAreaView>
+        <FlatList
+          data={this.state.data}
+          renderItem={({ item }) => (
+            <ListItem
+              leftAvatar={{ source: item.image}}
+              title={`${item.name}`}
+              subtitle={`$${item.price}`}
+              containerStyle={styles.itemContainer}
+              titleStyle={styles.itemText}
+              subtitleStyle={styles.itemText}
+              onPress={() => this.addToOrder()}
+            />
+          )}
+          keyExtractor={item => item.name}
+          ItemSeparatorComponent={this.renderSeparator}
+          ListHeaderComponent={this.renderHeader}
+        />
       </SafeAreaView>
     );
 
@@ -66,31 +139,20 @@ export default class DrinkMenu extends Component {
 }
 
 const styles = StyleSheet.create({
-  cardBackground: { 
-    backgroundColor:'#1c1c1c'
+  itemContainer:{
+    backgroundColor: "#181818"
   },
-  cardContainer:{
-    backgroundColor:'#3d3d3d', 
-    borderColor:"#b3b3b3", 
-    borderRadius: 20, 
-    borderWidth: 2,
-    marginBottom: 20
+  itemText:{
+    color: "white"
   },
-  titleContainer:{
-    backgroundColor:"#5c4646",
-    padding:10
+  loading: { 
+    backgroundColor: "#1c1c1c", 
+    flex: 1,
+    alignItems: "center", 
+    justifyContent: "center" 
   },
-  titleStyle: {
-    color:"#ffffff", 
-    fontSize:20
+  separator:{
+    height: 2,
+    backgroundColor: "#A79E9C",
   },
-  title:{
-    fontSize:25,
-    color:"#ffffff"
-  },
-  image:{
-    resizeMode: 'contain',
-    borderRadius: 20,
-    marginLeft:20
-  }
 });
--- app/additionalPages/FoodMenu.js
+++ app/additionalPages/FoodMenu.js
@@ -1,65 +1,137 @@
-import React, {Component} from 'react';
-import { Text, StyleSheet, View, SafeAreaView, SectionList, TouchableOpacity} from 'react-native';
-import { Card } from 'react-native-elements';
-
-const data = [
-  {
-    title:"Your Favorites",
-    data:[
-      {key:1, name:"Apple Slices", image:require("./img/apple_slices.jpg"), price: "0.50"},
-    ]
-  },
-  {
-    title:"All Food",
-    data:[
-      {key:1, name:"Apple Slices", image:require("./img/apple_slices.jpg"), price: "0.50"},
-      {key:2, name:"Carrots", image:require("./img/carrots.jpg"), price: "0.50"},
-    ]
-  }
-]
-
+import React, {Component} from "react";
+import {FlatList, StyleSheet, View, SafeAreaView, ActivityIndicator} from "react-native";
+import {SearchBar, ListItem} from "react-native-elements";
 
 export default class FoodMenu extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      loading: false,
+      data: [],
+      error: null,
+    };
+
+    // Delete this after API is working, just have this data to demonstrate how the views are working.
+    this.allFood = [
+      {key:1, name:"Apple Slices", image:require("./img/apple_slices.jpg"), price: "0.50"},
+      {key:2, name:"Carrots", image:require("./img/carrots.jpg"), price: "0.50"},
+      {key:3, name:"Apple Slices1", image:require("./img/apple_slices.jpg"), price: "0.50"},
+      {key:4, name:"Carrots1", image:require("./img/carrots.jpg"), price: "0.50"},
+      {key:5, name:"Apple Slices2", image:require("./img/apple_slices.jpg"), price: "0.50"},
+      {key:6, name:"Carrots2", image:require("./img/carrots.jpg"), price: "0.50"},
+      {key:7, name:"Apple Slices3", image:require("./img/apple_slices.jpg"), price: "0.50"},
+      {key:8, name:"Carrots3", image:require("./img/carrots.jpg"), price: "0.50"},
+      {key:9, name:"Apple Slices4", image:require("./img/apple_slices.jpg"), price: "0.50"},
+      {key:10, name:"Carrots4", image:require("./img/carrots.jpg"), price: "0.50"},
+      {key:11, name:"Apple Slices5", image:require("./img/apple_slices.jpg"), price: "0.50"},
+      {key:12, name:"Carrots5", image:require("./img/carrots.jpg"), price: "0.50"},
+      {key:13, name:"Apple Slices6", image:require("./img/apple_slices.jpg"), price: "0.50"},
+      {key:14, name:"Carrots6", image:require("./img/carrots.jpg"), price: "0.50"},
+      {key:15, name:"Apple Slices7", image:require("./img/apple_slices.jpg"), price: "0.50"},
+      {key:16, name:"Carrots7", image:require("./img/carrots.jpg"), price: "0.50"},
+    ];
   }
+
+  // This method is invoked once after the native UI for this component has finished rendering. This will
+  // automatically load all the drinks from the API once the screen is loaded.
+  componentDidMount(){
+    this.makeRemoteRequest();
+  }
+
+
+  makeRemoteRequest = () => {
+    this.setState({loading: true});
+
+    // Delete this statement after API is working.
+    this.setState({loading: false, data: this.allFood, error: null})
+
+    // Once we get the api set up, retrieve list of all food and save to local list:
+    // const url = `https://<url to get list of food>`;
+    // this.setState({ loading: true });
+
+    // fetch(url)
+    //   .then(res => res.json())
+    //   .then(res => {
+    //     this.setState({
+    //       data: res.results,
+    //       error: res.error || null,
+    //       loading: false,
+    //     });
+    //     this.allFood = res.results;
+    //   })
+    //   .catch(error => {
+    //     this.setState({ error, loading: false });
+    //   });
+  };
 
   addToOrder(){
     alert("TODO: Add navigation to popup to look at nutrition and add to order.")
   }
 
-  render() {
-		return (
-      <SafeAreaView style={{backgroundColor: 'grey'}}>
-        <SectionList
-            sections={data}
-            
-            // Generating section headers
-            renderSectionHeader={({section}) => {
-              return (
-                <View style={styles.titleContainer}>
-                  <Text style={styles.title}>
-                    {section.title}
-                  </Text>
-                </View>
-              )
-            }}
+  searchFilterFunction = text => {
+    this.setState({
+      value: text,
+    });
 
-            // Generating items for each section
-            renderItem={({item}) => {
-              return (
-                <TouchableOpacity  style={styles.cardBackground} onPress={() => this.addToOrder()}>
-                  <Card imageProps={styles.image} containerStyle={styles.cardContainer} titleStyle={styles.titleStyle}
-                  image={item.image}
-                  title={item.name}>
-                    <Text style={{textAlign: 'center', color:'#ffffff', fontSize: 20}}>
-                      Price: ${item.price}
-                    </Text>
-                  </Card>
-                </TouchableOpacity>
-              )
-            }}/>
+    const newData = this.allFood.filter(item => {
+      const itemData = `${item.name.toUpperCase()}`;
+      const textData = text.toUpperCase();
+
+      return itemData.indexOf(textData) > -1;
+    });
+    
+    this.setState({
+      data: newData,
+    });
+  };
+
+  renderHeader = () => {
+    return (
+      <SearchBar
+        placeholder="Type Here..."
+        onChangeText={text => this.searchFilterFunction(text)}
+        autoCorrect={false}
+        value={this.state.value}
+      />
+    );
+  };
+
+  renderSeparator = () => {
+    return (
+      <View
+        style={styles.separator}
+      />
+    );
+  };
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <SafeAreaView style={styles.loading}>
+          <ActivityIndicator />
+        </SafeAreaView>
+      );
+    }
+		return (
+      <SafeAreaView>
+        <FlatList
+          data={this.state.data}
+          renderItem={({ item }) => (
+            <ListItem
+              leftAvatar={{ source: item.image}}
+              title={`${item.name}`}
+              subtitle={`$${item.price}`}
+              containerStyle={styles.itemContainer}
+              titleStyle={styles.itemText}
+              subtitleStyle={styles.itemText}
+              onPress={() => this.addToOrder()}
+            />
+          )}
+          keyExtractor={item => item.name}
+          ItemSeparatorComponent={this.renderSeparator}
+          ListHeaderComponent={this.renderHeader}
+        />
       </SafeAreaView>
     );
 
@@ -67,31 +139,20 @@ export default class FoodMenu extends Component {
 }
 
 const styles = StyleSheet.create({
-  cardBackground: { 
-    backgroundColor:'#1c1c1c'
+  itemContainer:{
+    backgroundColor: "#181818"
   },
-  cardContainer:{
-    backgroundColor:'#3d3d3d', 
-    borderColor:"#b3b3b3", 
-    borderRadius: 20, 
-    borderWidth: 2,
-    marginBottom: 20
+  itemText:{
+    color: "white"
   },
-  titleContainer:{
-    backgroundColor:"#5c4646",
-    padding:10
+  loading: { 
+    backgroundColor: "#1c1c1c", 
+    flex: 1,
+    alignItems: "center", 
+    justifyContent: "center" 
   },
-  titleStyle: {
-    color:"#ffffff", 
-    fontSize:20
+  separator:{
+    height: 2,
+    backgroundColor: "#A79E9C",
   },
-  title:{
-    fontSize:25,
-    color:"#ffffff"
-  },
-  image:{
-    resizeMode: 'contain',
-    borderRadius: 20,
-    marginLeft:20
-  }
 });
