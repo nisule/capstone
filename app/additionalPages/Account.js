d8f13a8882e7d8dc214493c2c36ec24ec6812bb9
Nicholas Sundvall <sundvanl@plu.edu>
2020-03-29 18:51:14 -0700

    change password elements put in place

--- app/additionalPages/Account.js
+++ app/additionalPages/Account.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
-import { Alert, Button, Text, View, SafeAreaView, StyleSheet, Dimensions, AsyncStorage} from 'react-native';
+import { Alert, Button, Text, TextInput, View, SafeAreaView, StyleSheet, Dimensions, AsyncStorage, Modal, TouchableOpacity} from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+import { green } from 'color-name';
 
 var viewWidth = Dimensions.get('window').width; 
+var viewHeight = Dimensions.get('window').height;
 
 class AccountSettings extends Component {
   constructor(props) {
@@ -11,9 +13,23 @@ class AccountSettings extends Component {
     this.state = {
       user_id: "",
       email: "",
-      balance: 0
+      balance: 0,
+      isModalVisible: false,
+      isChangePasswordVisible: true,
+      currentPassword: "",
+      newPassword: "",
+      newPassword2: "",
+      text: "",
     }
   }
+
+  setModalVisible(visible) {
+    this.setState({isModalVisible: visible});
+  };
+
+  setChangePasswordVisibile(visible) {
+    this.setState({isChangePasswordVisible: visible});
+  };
 
   retrieveUserData = async () => {
     try {
@@ -33,13 +49,73 @@ class AccountSettings extends Component {
     this.retrieveUserData();
 
     return (
-        <SafeAreaView style={{flex: 1, backgroundColor: '#181818'}}>
+        <View 
+          style={{flex: 1, backgroundColor: '#181818'}}
+          >
+
+              <Modal
+              animationType="slide"
+              transparent={true}
+              visible={this.state.isModalVisible}
+              onRequestClose={() => {
+                this.setModalVisible(false);
+                this.setChangePasswordVisibile(true);
+              }}>
+
+              
+                <View style={styles.container}>
+                  <View style={styles.textInputContainer}>
+                    <TextInput
+                      style={styles.signupBoxes}
+                      placeholder="Current password"
+                      placeholderTextColor='white'
+                      onChangeText={(text) => this.setState({currentPassword: text})}
+                      passwordInput={this.state.text}
+                      secureTextEntry
+
+                      returnKeyType = { "next" }
+                      onSubmitEditing={() => { this.Password.focus(); }}
+                      blurOnSubmit={false}
+                      ref={(input) => { this.currentPassword = input; }}
+                    />
+                    <TextInput
+                      style={styles.signupBoxes}
+                      placeholder="New password"
+                      placeholderTextColor='white'
+                      onChangeText={(text) => this.setState({newPassword: text})}
+                      passwordInput={this.state.text}
+                      secureTextEntry
+
+                      returnKeyType = { "next" }
+                      onSubmitEditing={() => { this.confirmPassword.focus(); }}
+                      blurOnSubmit={false}
+                      ref={(input) => { this.Password = input; }}
+                    />
+                    <TextInput
+                      style={styles.signupBoxes}
+                      placeholder="Confirm Password"
+                      placeholderTextColor='white'
+                      onChangeText={(text) => this.setState({newPassword2: text})}
+                      passwordInput={this.state.text}
+                      secureTextEntry
+                      //onSubmitEditing={() => { this._touchable.touchableHandlePress() }}
+                      ref={(input) => { this.confirmPassword = input; }}
+                      // TODO: TRY TO MAKE KEYBOARD NOT SQUISH THE VIEWS, THEN DO PASSWORD CHECKS AND API CALL
+                    />
+                    
+                    <Button 
+                      title="Confirm password change"
+                      color="#fbba37"
+                    />
+                  </View>
+                </View>
+              
+            </Modal>
+            
 
             <View style={styles.titleStyle}>
                 <Text style={styles.titleText}>Your Account</Text>
-            </View>
-
-            <ScrollView>
+            </View>  
 
               <View style={styles.viewStyle}>
                 <Text style={styles.textStyle}>
@@ -59,17 +135,24 @@ class AccountSettings extends Component {
                 </Text>
               </View>
 
-            </ScrollView>
-
+            
             <View style={styles.buttonStyle}>
+              {
+                this.state.isChangePasswordVisible ?
                 <Button
                   title="Change Password"
-                  onPress={() => Alert.alert('Add popup window to create new password.')}
+                  onPress={() => {
+                    this.setModalVisible(true);
+                    this.setChangePasswordVisibile(false);
+                  }}
                   color="#fbba37"
                 />
-              </View>
-              
-        </SafeAreaView>
+                : null
+              }
+            </View>
+
+            
+        </View>
     );
   }
 }
@@ -94,10 +177,28 @@ const styles = StyleSheet.create({
     borderColor: 'grey',
   },
   buttonStyle: {
+    flex: 0.1,
     justifyContent: "flex-end",
     marginLeft: viewWidth * 0.3,
     marginRight: viewWidth * 0.3,
     marginBottom: 10,
+  },
+  signupBoxes: {
+    flex: 0.09,
+    fontSize: 18,
+    backgroundColor: '#303030',
+    color:'white',
+    borderRadius: 6,
+    alignItems: 'center',
+  },
+  container: {
+    height: viewHeight / 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: viewHeight * .3,
+  },
+  textInputContainer: {
+    flex: 1
   }
 })
 
